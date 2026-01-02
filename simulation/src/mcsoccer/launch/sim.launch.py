8a887ff95bf1ade2f4d87ac52921f4657e8b1065
notnot <hardiononadyash@gmail.com>
2026-01-02 18:10:47 +0700

    Clean up

--- simulation/src/mcsoccer/launch/sim.launch.py
+++ simulation/src/mcsoccer/launch/sim.launch.py
@@ -8,28 +8,25 @@ from launch_ros.actions import Node
 def generate_launch_description():
     pkg_name = 'mcsoccer' 
 
-    # 1. RSP (Robot State Publisher)
+    # Robot State Publisher
     rsp = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory(pkg_name), 'launch', 'rsp.launch.py'
         )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # 2. Gazebo (Ignition / Gz Sim)
+    # Gazebo
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'
         )]), launch_arguments={'gz_args': '-r empty.sdf'}.items()
     )
-
-    # 3. Spawn Robot
+    
     spawn_entity = Node(package='ros_gz_sim', executable='create',
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'mcsoccer',
-                                   '-z', '0.1'], # Spawn agak tinggi sedikit
+                                   '-z', '0.1'],
                         output='screen')
-
-    # 4. ROS-GZ Bridge 
   
     bridge = Node(
         package='ros_gz_bridge',
@@ -40,7 +37,7 @@ def generate_launch_description():
         output='screen'
     )
 
-    # 5. RViz2
+    # RViz2
     rviz = Node(
         package='rviz2',
         executable='rviz2',
