b1d82f9518f211a7303a64d4507dd686f37aaa16
notnot <hardiononadyash@gmail.com>
2026-01-02 18:09:26 +0700

    Clean up

--- simulation/src/mcsoccer/launch/joystick.launch.py
+++ simulation/src/mcsoccer/launch/joystick.launch.py
@@ -4,18 +4,14 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    
-    # Ambil lokasi file konfigurasi joystick.yaml
     joy_params = os.path.join(get_package_share_directory('mcsoccer'), 'config', 'joystick.yaml')
 
-    # 1. Node Driver Joystick (Membaca hardware)
     joy_node = Node(
         package='joy',
         executable='joy_node',
         parameters=[joy_params],
     )
-
-    # 2. Node Teleop (Menerjemahkan tombol jadi cmd_vel)
+    
     teleop_node = Node(
         package='teleop_twist_joy',
         executable='teleop_node',
